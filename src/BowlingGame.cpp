7fbe121e39672576fcdb21e9be8006410eb87d05
Suraj Nagargoje <91446342+surajnagargoje05@users.noreply.github.com>
2025-03-24 18:58:34 +0530

    Update BowlingGame.cpp

--- src/BowlingGame.cpp
+++ src/BowlingGame.cpp
@@ -1,1 +1,42 @@
 
+// BowlingGame.cpp
+#include "../include/BowlingGame.h"
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Constructs a new BowlingGame object.
+ */
+BowlingGame::BowlingGame() { rolls.reserve(21); }
+
+/**
+ * @brief Records the number of pins knocked down in a single roll.
+ * @param pins The number of pins knocked down.
+ */
+void BowlingGame::roll(int pins) { rolls.push_back(pins); }
+
+/**
+ * @brief Calculates and returns the total score of the game.
+ * @return The total score.
+ */
+int BowlingGame::getScore() {
+  Frame frame;
+  int score = 0;
+  int rollIndex = 0;
+
+  for (int frameNum = 0; frameNum < 10; frameNum++) {
+    if (frame.isStrike(rollIndex, rolls)) {
+      score += 10 + frame.strikeBonus(rollIndex, rolls);
+      rollIndex += 1;
+    } else if (frame.isSpare(rollIndex, rolls)) {
+      score += 10 + frame.spareBonus(rollIndex, rolls);
+      rollIndex += 2;
+    } else {
+      score += frame.sumOfBallsInFrame(rollIndex, rolls);
+      rollIndex += 2;
+    }
+  }
+  return score;
+}
